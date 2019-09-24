393f72378195fbb2b33399313a18beb2b83602de
happyinfrance10 <alvinduong2000@gmail.com>
2019-09-23 21:36:51 -0700

    Connect page rough draft done

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -13,7 +13,7 @@ export default class Button extends Component{
     selectBtn(){
       document.getElementById('writer').backgroundColor="#b3e7fa";
       document.getElementById('artist').backgroundColor="#b3e7fa";
-      document.getElementById('developer').backgroundColor="#b3e7fa";
+      document.getElementById('developer').backgroundColor="#66cef5";
       document.getElementById('sponsor').backgroundColor="#b3e7fa";
       document.getElementById('other').backgroundColor="#b3e7fa";
       this.backgroundColor="#66cef5";
@@ -21,7 +21,7 @@ export default class Button extends Component{
 
     render(props){
         return(
-            <div className="button" onClick={(e) => this.selectBtn(e)} id={this.props.label}>{this.props.label}</div>
+            <button className="button" onClick={(e) => this.selectBtn(e)} id={this.props.label}>{this.props.label}</button>
         )
     }
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,18 +9,21 @@ export default class Contact extends Component{
     constructor(props){
         super(props);
         this.state={
-
+            apply: false
         }
     }
 
     render(props){
         return(
             <Fade>
-            <div style={{overflow: "auto"}} className="card">
+            <div style={{overflow: "auto"}} className="card" id="contact-card">
               <h3 className="title">Contact Us</h3>
               <p>I am a...</p>
               <ButtonList />
-              <textarea id="message"/>
+              <p>Tell us about an issue in which you hold a contrarian view.</p>
+              <textarea id="response1"/>
+              <p>Describe a recent project or side-hustle.</p>
+              <textarea id="response2"/>
               <button id="submitBtn">Submit</button>
             </div>
             </Fade>
