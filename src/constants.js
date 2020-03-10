a386ddd5e298e18474552db2df5dbd77ce1f5598
17tangs <samtang1430@gmail.com>
2020-03-09 19:53:46 -0700

    changed constant

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
 const host = 'https://br-ghost.herokuapp.com';
 const key = '876bb28735b6e13c96024fd082';
-const url = 'http://localhost:3001';
-
+const url = 'https://master.d3444to1ugvoot.amplifyapp.com';
 export {host, key, url};
