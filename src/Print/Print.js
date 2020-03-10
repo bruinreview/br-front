9f9324873b8c195d2126aa20d6d0e2ddd533881a
17tangs <samtang1430@gmail.com>
2020-03-09 19:45:32 -0700

    minor

--- src/Print/Print.js
+++ src/Print/Print.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../Header';
 import SearchBar from '../Components/SearchBar';
 import Footer from '../Footer';
+import Zoom from 'react-reveal';
 import PrintEdition from './PrintEdition';
 import './Print.css'
 import p1 from '../resources/print1.png';
@@ -19,18 +20,24 @@ export default class Print extends Component{
           <div className="flex justify-center home">
               <Header />
                   <div className="main flex items-start flex-row">
+                    <Zoom>
                       <a target="_blank" href="https://elasticbeanstalk-us-west-1-133954069817.s3-us-west-1.amazonaws.com/BRInauguralEdition.pdf" className="posterCard">
                           <div  style={{backgroundImage:`url(${p1})`}} className="poster"/>
                           <div className="posterTitle"> Bruin Review Inaugural Edition</div>
                       </a>
+                    </Zoom>
+                    <Zoom>
                       <a target="_blank" href="https://elasticbeanstalk-us-west-1-133954069817.s3-us-west-1.amazonaws.com/News+in+Review+Fall+2019+v2.pdf" className="posterCard">
                           <div  style={{backgroundImage:`url(${p2})`}} className="poster"/>
                           <div className="posterTitle"> News in Review Fall 2019</div>
                       </a>
+                    </Zoom>
+                    <Zoom>
                       <a target="_blank" href="https://elasticbeanstalk-us-west-1-133954069817.s3-us-west-1.amazonaws.com/Flagship+III+(Winter+2020)+(2_7).pdf" className="posterCard">
                           <div  style={{backgroundImage:`url(${p3})`}} className="poster"/>
                           <div className="posterTitle"> Flagship III Winter 2020</div>
                       </a>
+                    </Zoom>
                   </div>
               <Footer />
 
