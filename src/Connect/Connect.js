8cd7388ad909a5c8754b1d458a714c1dda20f4f6
17tangs <samtang1430@gmail.com>
2020-04-07 18:00:16 -0400

    added recruitment and logo

--- src/Connect/Connect.js
+++ src/Connect/Connect.js
@@ -6,6 +6,8 @@ import About from './Components/About';
 import Contact from './Components/Contact';
 import GetInvolved from './Components/GetInvolved';
 import Support from './Components/Support';
+import Zoom from 'react-reveal/Zoom';
+import p1 from '../resources/br-about.png';
 
 
 export default class Print extends Component{
