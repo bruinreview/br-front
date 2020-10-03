f46ec473d4094f31fcec0d13f679f0393ccce130
17tangs <samtang1430@gmail.com>
2020-10-03 14:54:40 -0700

    mobile about view fixed

--- src/Connect/Apply.js
+++ src/Connect/Apply.js
@@ -41,6 +41,22 @@ export default class Apply extends Component {
         <div className="flex justify-center home">
           <MobileNav showBar={false} />
           <div className="mobileAbout">
+            <h3 className="mobileAboutTitle">Welcome to the Bruin Review</h3>
+            <hr />
+            We're a society of independent thinkers who care about bringing
+            discourse back to UCLA and we want you to join us. Fall applications
+            are now open! Here are the important dates:
+            <li>
+              Info Session: October 7 @ 7 pm on Janns w/ masks (Zoom Info
+              Session @ 8:30 pm)
+            </li>
+            <li>Applications Due: October 11 @ 11:59 pm</li>
+            You can check out our complete works under the "Print" tab and read
+            our articles on the home page. These only scrape the surface of what
+            we do here, but they're a great example of how our six departments
+            work together to promote diversity of thought. Click the pdf to
+            learn more!
+            <div className="pv4" />
             <h3 className="mobileAboutTitle">Apply to the Bruin Review</h3>
             <hr />
             <div
@@ -99,6 +115,46 @@ export default class Apply extends Component {
                 Apply Now
               </div>
             </a>
+            <div className="pv4" />
+            <h3 className="mobileAboutTitle">About the Bruin Review</h3>
+            <hr />
+            <p>
+              The University was once a place of eclectic thought – the
+              gathering of bright minds to encourage the exchange, deliberation,
+              and inspection of ideas. Today, the inverse is true. Universities
+              are proxy for a narrow viewpoint that is easily predictable and
+              left unquestioned. Ideas are no longer submitted as subjects of
+              observation and scrutiny; rather, they are endowed to students as
+              delicacies to be defended at any cost.
+            </p>
+            <p>
+              The Bruin Review was founded to end this soft treatment of ideas.
+              Thought should be forged, not coddled. The University has lost its
+              edge as a tool for truth. Instead, it is a cog in the wheel of
+              ideology. The Review aims to bring back those tools for the
+              scrutiny of thought. To learn is to be curious, and to be curious
+              is to question. The Review is here to question.
+            </p>
+            <p>
+              Universities should be less similar to a cultivated garden of
+              monolithic ideals and more like a battleground of beliefs. Buckle
+              up, because we plan to pick fights where dissension is rarely
+              fostered and disputation seldom tolerated. Contention is our
+              middle name.
+            </p>
+            <p>
+              The Bruin Review will always push to be on the edge of truth, at
+              times nearly falling off. Our goal is to present diverse arguments
+              on all applicable topics, transcendent of partisan semantics.
+              First and foremost, we are a society of those who enjoy civil
+              discourse and pushing the limits of their intellect. Second, we
+              are a medium for any responsible message. Our publication does not
+              exist to bolster rumors, gossip, or pontificate over national
+              politics. We hope to bring forward dialogue on issues which affect
+              students’ lives and challenge the status quo of ideas. Not through
+              simple debate, but by productive and sympathetic discussion of
+              things sparsely mentioned.
+            </p>
           </div>
         </div>
       );
